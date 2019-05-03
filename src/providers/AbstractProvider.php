0318395524727ab6eb9a39a2ecef59c436294115
Gareth Drew <garethdrew@gmail.com>
2019-05-03 07:58:04 +0100

    Some initial ideas

--- src/providers/AbstractProvider.php
+++ src/providers/AbstractProvider.php
@@ -2,6 +2,9 @@
 
 abstract class AbstractProvider
 {
+
+    protected $emails = [];
+
     public function setEmails(Array $emails) : void {
         // Should take a list of emails and add to list
     }
--- src/providers/Sendgrid.php
+++ src/providers/Sendgrid.php
@@ -4,5 +4,33 @@ namespace Worzy\Providers;
 
 class Sendgrid extends AbstractProvider
 {
+    /*
+    {
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "john@example.com"
+        }
+      ],
+      "subject": "Hello, World!"
+    }
+  ],
+  "from": {
+    "email": "from_address@example.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, World!"
+    }
+  ]
+}
+*/
 
+
+
+    public function send() {
+
+    }
 }
